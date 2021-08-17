c06b5f06d465d1377690fc58105506046fdef279
Ben Clabby <benclabby@gmail.com>
2021-08-17 15:08:05 -0400

    Include dist

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+export{a as ChainUnsupportedError,C as ConnectionRejectedError,b as ConnectorUnsupportedError,U as UseWalletProvider,c as default,u as useWallet}from"./index-ee5bd941.js";import"react";import"events";
+//# sourceMappingURL=index.js.map
